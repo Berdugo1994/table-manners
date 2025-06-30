4f1de62771710bf3a70ce792a5fb09fa5e9e0af5
Eden Berdugo <edenbe@monday.com>
2025-07-01 02:24:54 +0300

    With click checker

--- app/components/board/boardMiddle.tsx
+++ app/components/board/boardMiddle.tsx
@@ -3,13 +3,28 @@ import styles from "./board.module.css";
 import { Chips, Credit } from "../icons/credit";
 import { usePlayerStore } from "@/app/store/playerStore";
 import { Divider } from "@heroui/react";
+import { useState } from "react";
 
 export const BoardMiddle = () => {
   const { getGameName, getRatio } = useBoardStore();
   const { getAllCredits } = usePlayerStore();
+  const [clickTimestamps, setClickTimestamps] = useState<number[]>([]);
+  const checkForDoubleClick = () => {
+    const now = Date.now();
+    const twoSecondsAgo = now - 2000;
+    const newTimestamps = [...clickTimestamps, now].filter(
+      (timestamp) => timestamp > twoSecondsAgo
+    );
+    setClickTimestamps(newTimestamps);
+    if (newTimestamps.length > 5) {
+      setTimeout(() => {
+        setClickTimestamps([]);
+      }, 10000);
+    }
+  };
 
   return (
-    <div className={styles.boardMiddle}>
+    <div className={styles.boardMiddle} onClick={checkForDoubleClick}>
       <div className={styles.boardMiddleText}>
         <div className={styles.boardMiddleTextTitle}>{getGameName()}</div>
       </div>
@@ -24,6 +39,9 @@ export const BoardMiddle = () => {
           <Chips />
         </div>
       </div>
+      {clickTimestamps.length > 5 && (
+        <div className="text-white">Bona Yamanyak!</div>
+      )}
     </div>
   );
 };
